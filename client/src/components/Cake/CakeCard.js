9e41c5fa4c7bed60f3c0562bec92eaa9fe0f61c7
Amelia Scarbrough <scarbroughm@hotmail.com>
2022-11-08 20:19:06 +0100

    add more card styles

--- client/src/components/Cake/CakeCard.js
+++ client/src/components/Cake/CakeCard.js
@@ -1,19 +1,36 @@
 import React, {useState, useEffect} from 'react'
-import { calcAveRating } from '../../hooks/math'
+import { calcAveRating, priceWorthMultiplier } from '../../hooks/math'
 
 
 const CakeCard = ({cake}) => {
+  const [isHovered, setIsHovered] = useState(false)
 
-const {cafe, city, country, price, texture, taste, presentation} = cake
+
+const {cafe, city, country, price, texture, taste, presentation, notes} = cake
 
 const rate = calcAveRating(price, texture, taste, presentation)
 
+const cakes = priceWorthMultiplier(price)
 
     return (
-        <div >
-          <h1>{cafe} in {city}, {country}</h1>
+        <div  
+        onMouseEnter={() => {
+          setIsHovered(true)}}
+        onMouseLeave={() => {
+          setIsHovered(false)
+        }}
+        >
+          {isHovered? 
+          <div className="text-contain">
+            <p></p>
+          <p>{notes}</p>
+          </div> :
+           <div className="text-contain">
+            <h1>{cafe} in {city}, {country}</h1>
           <p>rating: {rate}</p>  
- 
+        <p>{price}</p>
+         </div>}
+    
             </div>
     )
 }
--- client/src/hooks/math.js
+++ client/src/hooks/math.js
@@ -1,9 +1,14 @@
 export const calcAveRating = (price, text, taste, pres) => {
 
 
-// add and weight
+// add and weight the overall rating
     const added = (price * 1.3) + text + (taste *1.7) + pres
 // round and divide by 4
     return (added / 4).toFixed(3) 
 
+}
+
+
+export const priceWorthMultiplier = (price) => {
+    return price - 2
 }
