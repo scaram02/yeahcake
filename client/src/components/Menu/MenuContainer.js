cd6a86548888b0894da29f36e17ca8d52a1c4788
Amelia Scarbrough <scarbroughm@hotmail.com>
2022-11-12 18:45:28 +0100

    menu styles

--- client/src/components/Menu/MenuContainer.js
+++ client/src/components/Menu/MenuContainer.js
@@ -22,7 +22,7 @@ const MenuContainer = ({user}) => {
         <NavLink id="home" activeClassName="active" className="menu-item" to="/feed">Home</NavLink>
         <NavLink id="rate" className="menu-item" activeClassName="active" to="/rate">Rate a cake!</NavLink>
         <NavLink id="my-cakes"  className="menu-item" activeClassName="active" to="/my-cakes">My cakes</NavLink>
-        <NavLink onClick={handleLogout} className="menu-item" activeClassName="active" to="/">Log out</NavLink>
+        <NavLink id="logout" onClick={handleLogout} className="menu-item" activeClassName="active" to="/">Log out</NavLink>
       </Menu>
     )
 }
